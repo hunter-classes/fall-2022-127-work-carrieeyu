66d1c2a2b89f1d807921b88bae00d2787a16b443
carrie yu <104949316+carrieeyu@users.noreply.github.com>
2022-09-01 12:47:19 +0000

    added t1.py

---
+++ code/turtles/t1.py
@@ -0,0 +1,15 @@
+import turtle 
+wn = turtle.Screen() #making turtle window
+
+crush = turtle.Turtle() #making a turtle
+
+crush.forward(5)
+crush.left(30)
+crush.forward(130)
+crush.right(50)
+crush.forward(70)
+
+wn.exitonclick()
+wn.mainloop()
+
+a=10+30
