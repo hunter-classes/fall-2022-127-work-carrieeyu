e9e2713ac7b0158dbade28949ce3bda167248207
carrie yu <104949316+carrieeyu@users.noreply.github.com>
2022-09-18 20:37:45 +0000

    updated bondify

--- additionalCode/examples.py
+++ additionalCode/examples.py
@@ -76,10 +76,21 @@ def bondify(name):
   Input string in the form “first last”
 	Return “Last, first last”
   """
+  result1 = ""
+  location = name.find(' ')
+  first = name[:location].capitalize() #everything before the space between last and first
+  last = name[location+1:].capitalize() #everything after the space between last and first
+
+  result1 = last + ", " + result1 + first + " " + last
+  
+  return result1
 
 
 result = initialize("james bond")
 print("james bond -->", result)
+
+result1 = bondify("james bond")
+print("james bond -->", result1)
 
 
 
