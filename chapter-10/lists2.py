93163ef7cbadaf7bbab59a20c2435f09534caa28
carrie yu <104949316+carrieeyu@users.noreply.github.com>
2022-10-02 05:39:17 +0000

    updated lists hw

--- chapter-10/lists2.py
+++ chapter-10/lists2.py
@@ -19,7 +19,7 @@ print("The average is",result,".")#should be 3
 result = average([4,5,10])
 print("The average is",result,".")#should be 6.3......
 
-#Number 6:Write a function sum_of_squares(xs) that computes the sum of the squares of the numbers in the list xs. For example, sum_of_squares([2, 3, 4]) should return 4+9+16 which is 29:
+#Number 6: Write a function sum_of_squares(xs) that computes the sum of the squares of the numbers in the list xs. For example, sum_of_squares([2, 3, 4]) should return 4+9+16 which is 29:
 
 def sum_of_squares(xs):
   sum = 0
