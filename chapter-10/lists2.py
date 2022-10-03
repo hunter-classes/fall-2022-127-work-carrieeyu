06a63569502fb484ab353e57b66448bd665af6ca
carrie yu <104949316+carrieeyu@users.noreply.github.com>
2022-10-03 13:37:36 +0000

    can piglatin a sentence

--- chapter-10/lists2.py
+++ chapter-10/lists2.py
@@ -1,7 +1,7 @@
 """due 10/3 before class"""
 
 #Number 4: Write a function called average that takes a list of numbers as a parameter and returns the average of the numbers.
-
+"""
 def average(listNums):
   sum = 0
   
@@ -9,6 +9,18 @@ def average(listNums):
     sum += num
 
   return sum/len(listNums)
+"""
+
+                                    #or
+  
+def average(l):
+  sum=0 
+  
+  for item in l:
+    sum += item
+
+  average = sum/len(l)
+  return average
 
 result = average([1,2,3])
 print("The average is",result,".")#should be 2
@@ -20,13 +32,20 @@ result = average([4,5,10])
 print("The average is",result,".")#should be 6.3......
 
 #Number 6: Write a function sum_of_squares(xs) that computes the sum of the squares of the numbers in the list xs. For example, sum_of_squares([2, 3, 4]) should return 4+9+16 which is 29:
-
+"""
 def sum_of_squares(xs):
   sum = 0
 
   for num in xs:
     sum += num**2
 
+  return sum
+"""
+
+def sum_of_squares(l):
+  sum=0
+  for item in l:
+    sum = sum + item*item
   return sum
 
 result = sum_of_squares([2,3,4])
