dd1b1e241a07433e3f28c260e8c0f971216279e4
carrie yu <104949316+carrieeyu@users.noreply.github.com>
2022-10-13 13:32:33 +0000

    updated #6

--- code/labs/listlab.py
+++ code/labs/listlab.py
@@ -68,10 +68,10 @@ def longerThanFive(string):
   if len(string) > 5:
     newString = string.upper()
   else:
-    newString = "string is not longer than 5 characters"
+    newString = "the string is not longer than 5 characters"
   return newString
 
-result = longerThanFive("woohoo no more homework")
+result = longerThanFive("no more homework")
 print("The upper case version of the word is,",result,".")
 
 result = longerThanFive("no")
@@ -106,32 +106,26 @@ print("------------------------------------")
 #Ex [1,2,3] and [10,20,30] would return the list [11,22,33]
 print("(***6)")
 
-def addLists(list1,list2):
-  newList = []
+def addLists(list1, list2):
+  num = 0
+  newList=[]
 
-  for num in list1:
-    for num in list2:
-      newList = list1[num+1] + list2[num+1]
+  for number in list1:
+    newList.append(number+list2[num])
+    num += 1
   return newList
-
-result = addLists([7,2,0,3],[1,2,3,4])
-print("The combined list of added numbers are",result,".")
-
-"""
-def addLists(list1,list2):
-  newList = []
   
-  for num in list1:
-    for num in list2:
-      newList = list1[num] + list2[num]
-    return newList
-
-result = addLists([7,2,0,3],[1,2,3,4])
-#result = addLists([2,11,5,6])
+result = addLists([4,1,0],[1,1,4])
 print("The combined list of added numbers are",result,".")
-"""
+
+result = addLists([3,1],[2,4])
+print("The combined list of added numbers are",result,".")
+
+result = addLists([1,8,1,10],[1,2,3,4])
+print("The combined list of added numbers are",result,".")
+
 #Number 7: Ch 10:#10; Count how many words in a list have length 5.
-print("(7)")
+print("(7, Ch 10:#10)")
 
 def countFive(numsList):
   count = 0
@@ -152,8 +146,9 @@ print("The number of words with a length of 5 is",result,".")
 
 print("------------------------------------")
 #Number 8: Ch 10:#11; Sum all the elements in a list up to but not including the first even number.
-print("(***8)")
+print("(***8, Ch 10:#11)")
 
+"""
 def noFirstEven(numsList):
 
   sum = 0
@@ -173,12 +168,13 @@ print("The sum without the first even number is",result,".")
 
 result = noFirstEven([2,3,43,1])#-->should return 47
 print("The sum without the first even number is",result,".")
-
+"""
 
 print("------------------------------------")
 #Number 9: Ch 10:#12; Count how many words occur in a list up to and including the first occurrence of the word “sam”.
-print("(***9)")
+print("(***9, Ch 10:#12)")
 
+"""
 def countToSam(listNums):
 
   count = 0;
@@ -196,3 +192,4 @@ print("The number of times sam occurs in the list is",result,".")
 
 result = countToSam(["sam","sam","what"])
 print("The number of times sam occurs in the list is",result,".")
+"""
