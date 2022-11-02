632bd7caf7ba31c2117a70b81aba0609d2537ac9
carrie yu <104949316+carrieeyu@users.noreply.github.com>
2022-11-02 00:27:59 +0000

    updated file and text

---
+++ code/project-01-madlibs/madlibstest.py
@@ -0,0 +1,21 @@
+#text file test
+
+
+
+import random
+
+verbs = ['talk','walk','nap','yell','scream']
+nouns = ['boy','girl','man','woman','cat']
+emotions = ['angry','sad','annoyed']
+
+f = open("madlibs.txt")
+
+def substitute(madlibs):
+  for word in f:
+    word = word.replace("<VERB>", random.choice(verbs))
+    word = word.replace("<NOUN>", random.choice(nouns))
+    word = word.replace("<EMOTION>", random.choice(emotions))
+  return word
+
+word = substitute("madlibs.txt")
+print(word) #add another extra and put \n later on to separate each sentence
