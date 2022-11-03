f4bf5383db59c4c29dfe622d01f5f9f33a959edf
carrie yu <104949316+carrieeyu@users.noreply.github.com>
2022-11-03 21:41:23 +0000

    updated madlibs

--- code/project-01-madlibs/madlibs.py
+++ code/project-01-madlibs/madlibs.py
@@ -6,7 +6,7 @@ MADLIBS EXTRAS:
 
 import random
 
-#list of story potential story replacements
+#creating a list of story elements to use as replacements
 verbs = ['talk','walk','run','yell','scream','kick','dance','sit','nap','learn','shout']
 nouns = ['school','home','hell','bus','mother','father','river','Bahamas','Hogwarts','snake']
 emotions = ['angry','sad','annoyed','happy','insane','disappointed','confident','desperate']
@@ -17,15 +17,16 @@ f = open("story.txt", "r") #accessing the text file
 theStory = f.read() #reading from the text file
 words = theStory.split()
 
+#this function is used to replace the indicated <> with story elements from the lists that are shown above 
 def substitute(madlibs):
   word = theStory
-  word = word.replace("<VERB>", random.choice(verbs))
-  word = word.replace("<NOUN>", random.choice(nouns))
-  word = word.replace("<EMOTION>", random.choice(emotions))
-  word = word.replace("<NAME>", random.choice(names))
-  return word
+  word = word.replace("<VERB>", random.choice(verbs)) #replaces any indication of <VERB> with an actual verb from the specified list
+  word = word.replace("<NOUN>", random.choice(nouns)) #same purpose
+  word = word.replace("<EMOTION>", random.choice(emotions)) #same purpose
+  word = word.replace("<NAME>", random.choice(names)) #same purpose
+  return word #returns all the indicated substitutions of the story
   
   
-print(substitute(words)) #displaying the story text with substitutions 
+print(substitute(words)) #displaying the story text with substitutions and prints out a new story
 f.close() #closing the text file
 
