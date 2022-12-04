ab1d234ad1cb25be1a80e180f67b97a138ed9031
carrie yu <104949316+carrieeyu@users.noreply.github.com>
2022-12-04 18:41:18 +0000

    testing

--- c++/intro.cpp
+++ c++/intro.cpp
@@ -21,6 +21,6 @@ to run:
 int main()  // here to end of line is a comment 
 {
   std::cout << "Hello World!" << std::endl; //will print Hello World when with "/a.out" as input in replt
-  
+  std::cout << "is this working?" << std::endl;
   return 0;
 }
